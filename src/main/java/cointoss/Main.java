c60be154302b65bd0272dcb4c94010b4f73f531d
Teletha <Teletha.T@gmail.com>
2020-12-25 18:16:50 +0900

    Test

--- src/main/java/cointoss/Main.java
+++ src/main/java/cointoss/Main.java
@@ -10,19 +10,25 @@
 package cointoss;
 
 import java.io.BufferedWriter;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.google.api.core.ApiFuture;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 
 import kiss.I;
 import kiss.JSON;
@@ -37,11 +43,22 @@ public class Main implements HttpFunction {
         BufferedWriter writer = response.getWriter();
         writer.write(bitmex());
 
-        Path path = Path.of(URI.create("gs://cointoss-function/2020-11-25-00.log"));
-        List<String> lines = Files.readAllLines(path, StandardCharsets.UTF_8);
-        for (String line : lines) {
-            writer.write(line + "\r\n");
-        }
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+        FirebaseOptions options = FirebaseOptions.builder().setCredentials(credentials).setProjectId("jackpot").build();
+        FirebaseApp.initializeApp(options);
+
+        Firestore db = FirestoreClient.getFirestore();
+        DocumentReference docRef = db.collection("users").document("alovelace");
+        // Add document data with id "alovelace" using a hashmap
+        Map<String, Object> data = new HashMap<>();
+        data.put("first", "Ada");
+        data.put("last", "Lovelace");
+        data.put("born", 1815);
+        // asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
+        // ...
+        // result.get() blocks on response
+        System.out.println("Update time : " + result.get().getUpdateTime());
     }
 
     private String bitmex() {
